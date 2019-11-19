faf9a04c6745d526ac91109109e03bd67dc760f3
successcentral123@gmail.com <ehfltkah21>
2019-11-19 13:06:37 -0500

    change adv_text to textField

--- AdvAPP/models.py
+++ AdvAPP/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class AdventureText(models.Model):
-    adv_text = models.CharField(max_length=500)
+    adv_text = models.textField()
 
 class ChoiceText(models.Model):
     choice_text = models.CharField(max_length=300)
     choice_of = models.ForeignKey(AdventureText, related_name='requested_choice', on_delete=models.CASCADE)
     result_text = models.ForeignKey(AdventureText, on_delete=models.CASCADE)
+
+# class Stories(models.Model):
