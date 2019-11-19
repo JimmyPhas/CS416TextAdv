1946d8ec9100f82d1e4dae134e93f48798bda6e8
JimmyPhas <JimmyPhasouk@gmail.com>
2019-11-19 14:16:28 -0500

    added veiws to html pages

--- AdvAPP/views.py
+++ AdvAPP/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def home(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
+
+def create(request):
+    template = loader.get_template('create.html')
+    return HttpResponse(template.render())
+
+def play(request):
+    template = loader.get_template('play.html')
+    return HttpResponse(template.render())
