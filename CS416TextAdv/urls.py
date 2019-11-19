247f9cdf9aaf0beeafbfee531a31cea083c65b70
JimmyPhas <JimmyPhasouk@gmail.com>
2019-11-19 14:16:54 -0500

    added link to urls.py in AdvAPP

--- CS416TextAdv/urls.py
+++ CS416TextAdv/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 # another test commit
 urlpatterns = [
+    path('AdvAPP/', include('AdvAPP.urls')),
     path('admin/', admin.site.urls),
 ]
