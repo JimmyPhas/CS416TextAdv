96ca0dc60993a7179ae57a4665a78f25b2686fa1
JimmyPhas <ehfltkah21>
2019-11-19 13:34:37 -0500

    test comment

--- CS416TextAdv/urls.py
+++ CS416TextAdv/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-# random comment
+# another test commit
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
