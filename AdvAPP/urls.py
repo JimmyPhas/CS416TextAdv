e2a8d164602a0262ad193716829facd9f039e30b
JimmyPhas <JimPhas@gmail.com>
2019-11-28 23:38:00 -0500

    added update and delete functionality

--- AdvAPP/urls.py
+++ AdvAPP/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('create/<int:story_id>/<int:adv_id>', views.create, name='create'),
     path('create/', views.create_story, name='create_story'),
     path('play/', views.play, name='play'),
-    path('start/<int:stories_id>/', views.start, name='start'),
     path('<int:result_text>/', views.playing, name='playing'),
+    path('edit/<str:story_title>/', views.edit, name='edit'),
+    path('delete/<str:del_type>/<int:id>', views.delete, name='delete'),
+    path('update/<str:up_type>/<int:id>', views.update, name='update'),
 ]
--- AdvAPP/views.py
+++ AdvAPP/views.py
@@ -11,17 +11,34 @@ def home(request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
 
-def create(request, story_id, adv_id):
+def edit(request, story_title):
+    story = Stories.objects.get(story_title=story_title)
+
+    context = {
+        'story' : story,
+    }
+    return render(request, 'edit.html', context)
+
+def create(request,story_id,adv_id):
     if request.method == 'POST':
         origin_story = Stories.objects.get(pk=story_id)
         branch_story = AdventureText.objects.get(pk=adv_id)
-        story_text = request.POST.get("text_input")
-        choice_text = request.POST.get("text_result")
-        new_text = AdventureText.objects.create(adv_text=story_text, story=origin_story)
-        ChoiceText.objects.create(choice_text=choice_text, choice_of=branch_story , result_text=new_text.pk)
-        return render(request, 'playing.html', {'adventureText': new_text})
+        if (request.POST.get("select_input") == "null"):
+            story_text = request.POST.get("text_input")
+            choice_text = request.POST.get("text_result")
+            new_text = AdventureText.objects.create(adv_text=story_text, story=origin_story)
+            ChoiceText.objects.create(choice_text=choice_text, choice_of=branch_story, result_text=new_text.pk)
+            return render(request, 'playing.html', {'adventureText': new_text})
+        else:
+            story_text = request.POST.get("select_input")
+            choice_text = request.POST.get("text_result")
+            new_text = AdventureText.objects.get(pk=story_text)
+            ChoiceText.objects.create(choice_text=choice_text, choice_of=branch_story , result_text=new_text.pk)
+            return render(request, 'playing.html', {'adventureText': new_text})
     else:
-        return render(request, 'create.html')
+        origin_story = Stories.objects.get(pk=story_id)
+        return render(request, 'create.html', { 'story' : origin_story})
+
 
 def create_story(request):
     if request.method == 'POST':
@@ -49,13 +66,6 @@ def play(request):
     }
     return HttpResponse(template.render(context, request))
 
-def start(request, stories_id):
-    try:
-        adventureText = AdventureText.objects.get(pk=stories_id)
-    except AdventureText.DoesNotExist:
-        raise Http404("Story does not exist")
-    return render(request, 'playing.html', {'adventureText': adventureText})
-
 def playing(request, result_text):
     try:
         adventureText = AdventureText.objects.get(pk=result_text)
@@ -63,4 +73,61 @@ def playing(request, result_text):
         raise Http404("Story does not exist")
     return render(request, 'playing.html', {'adventureText': adventureText})
 
+def delete(request, del_type, id):
+    if del_type == "story":
+        story = Stories.objects.get(pk=id)
+        story.delete()
+    elif del_type == "adventure":
+        adv_text = AdventureText.objects.get(pk=id)
+        adv_text.delete()
+    elif del_type == "choice":
+        choice_text = ChoiceText.objects.get(pk=id)
+        choice_text.delete()
+    return render(request, 'delete.html', { 'del_type' : del_type })
+
+def update(request, up_type, id):
+    if request.method == 'POST':
+        if up_type == "story":
+            story = Stories.objects.get(pk=id)
+            story.story_title = request.POST.get("story_title")
+            story.author = request.POST.get("story_author")
+            story.save()
+            context = {
+                'story': story,
+            }
+        elif up_type == "adventure":
+            adv_text = AdventureText.objects.get(pk=id)
+            adv_text.adv_text = request.POST.get("text_input")
+            adv_text.save()
+            context = {
+                'story': adv_text.story,
+            }
+        elif up_type == "choice":
+            choice_text = ChoiceText.objects.get(pk=id)
+            choice_text.choice_text = request.POST.get("text_result")
+            choice_text.save()
+            context = {
+                'story': choice_text.choice_of.story,
+            }
+        return render(request, 'edit.html', context)
+    else:
+        if up_type == "story":
+            story = Stories.objects.get(pk=id)
+            context = {
+                'del_type': up_type,
+                'story' : story
+            }
+        elif up_type == "adventure":
+            adv_text = AdventureText.objects.get(pk=id)
+            context = {
+                'del_type': up_type,
+                'adv_text' : adv_text
+            }
+        elif up_type == "choice":
+            choice_text = ChoiceText.objects.get(pk=id)
+            context = {
+                'del_type': up_type,
+                'choice_text' : choice_text
+            }
+        return render(request, 'update.html', context)
 
